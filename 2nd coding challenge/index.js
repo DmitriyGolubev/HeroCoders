69b77ba5fe016808b9aa01f4d0b2f6a32eda8325
Dimasik <6609646@gmail.com>
2021-12-07 14:10:30 +0300

    more than fits on one page fixed

--- 2nd coding challenge/index.js
+++ 2nd coding challenge/index.js
@@ -17,12 +17,24 @@ get(baseUrl, function (res){
     var data = [];
     res.forEach(function (component) {
         if (component.lead === undefined) {
-            get('https://herocoders.atlassian.net/rest/api/3/search?jql=project = IC AND component ="' + component.name + '"', function(d) {
-                data.push(component.name);
-                data.push(d.issues.length);
-
-                console.log(data);
-            })
+            load(component, data, 0)
         }
     });
 });
+
+
+function load(component, data, startAt) {
+    get('https://herocoders.atlassian.net/rest/api/3/search?jql=project = IC AND component ="' + component.name + '"&startAt=' + startAt, function(d) {
+        if (data[component.name]) {
+            data[component.name] += d.issues.length;
+        } else {
+            data[component.name] = d.issues.length;
+        }
+
+        if (d.issues.length == 50) {
+            load(component, data, startAt + 50);
+        } else {
+            console.log(data);
+        }
+    })
+}
